403b82391a8e4d085dadd140763df4c6adcfb58c
Stulle Ich <stulleamgym@gmx.net>
2009-11-08 23:18:22 +0100

    Added: Ionix advanced (multiuser) webserver [iOniX/Aireoreion/wizard/leuk_he] - Stulle + You must select the multi user template to enable the multi user settings. Added: Multiuser WebInterface Cookie settings [Aireoreion] - Stulle Added: MorphXT's failed login handling for WebInterface [MorphXT/leuk_he/dreamwalker] - Stulle Added: Failed login screen for WebInterface [SiRoB/CommanderGer] - Stulle

--- srchybrid/Preferences.h
+++ srchybrid/Preferences.h
@@ -1176,6 +1176,8 @@ public:
 
 	static bool		m_bDateFileNameLog; // Date File Name Log [AndCycle] - Stulle
 
+	static	bool	m_bIonixWebsrv; // Ionix advanced (multiuser) webserver [iOniX/Aireoreion/wizard/leuk_he] - Stulle
+
 	static bool		m_bCloseEasteregg; // Diabolic Easteregg [Stulle] - Mephisto
 
 	// ==> Mephisto Upload - Mephisto
@@ -2624,6 +2626,8 @@ public:
 	// <== Advanced Transfer Window Layout [Stulle] - Stulle
 
 	static	bool	DateFileNameLog()		{ return m_bDateFileNameLog;} // Date File Name Log [AndCycle] - Stulle
+
+	static	bool	UseIonixWebsrv()		{ return m_bIonixWebsrv; } // Ionix advanced (multiuser) webserver [iOniX/Aireoreion/wizard/leuk_he] - Stulle
 
 	// ==> Mephisto Upload - Mephisto
 	static uint8	GetMinSlots()			{return m_uMinSlots;}
